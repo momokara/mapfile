b518e9c8d8bbb68cfa8e46bb21197f74d0dcd152
momokara <1125076@qq.com>
2020-01-11 16:42:38 +0800

    添加输入提示

--- bin/menu.js
+++ bin/menu.js
@@ -2,6 +2,7 @@
 
 // 加个运行目录方便调用
 let inquirer = require('inquirer');
+inquirer.registerPrompt('suggest', require('inquirer-prompt-suggest'));
 const path = require('path');
 
 const { savefilemap } = require('./dirTools');
@@ -26,11 +27,23 @@ const logmenu = () => {
     switch (res.todo) {
       case 'getTree':
         let dir = path.resolve('./');
-        let output = path.resolve('./','FILEMAP.md');
+        let output = path.resolve('./', 'FILEMAP.md');
         // 获取文件列表需要的信息
         let getFileTreeMenu = [
-          { message: '要查看的文件目录(以~开头会替换成 [当前命令行目录/])', name: 'dir', default: dir },
-          { message: '输出地址(以~开头会替换成命令行显示的路径,带斜杠)', name: 'output', default: output }
+          {
+            type: "suggest",
+            message: '要查看的文件目录(以~开头会替换成 [当前命令行目录/])',
+            name: 'dir',
+            suggestions: [dir],
+            default: dir
+          },
+          {
+            type: "suggest",
+            message: '输出地址(以~开头会替换成命令行显示的路径,带斜杠)',
+            name: 'output',
+            suggestions: [output],
+            default: output
+          }
         ]
         showMenu(getFileTreeMenu).then(res => {
           let _useDir = res.dir;
